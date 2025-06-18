7d93c4842b725e57fdc2b9d7b88bba82babf1c33
Eduarda Leal <edudardaleal753@gmail.com>
2025-06-18 17:55:10 -0300

    Começanco projeto

---
+++ database/migrations/tenant/2025_06_17_235538_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->decimal('value', 10, 2);
+            $table->string('cpf');
+            $table->string('document_path')->nullable();
+            $table->enum('status', ['Em processamento', 'Aprovada', 'Negada']);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
